985e49830a373560eb8dfdc93a92e92860a89073
WaquarShaikh5555 <waquarsk1@gmail.com>
2025-04-18 16:40:25 +0530

    Fixed typo and updated bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-krequire('dotenv').config();
+require('dotenv').config();
 const { Client, GatewayIntentBits, Collection, REST, Routes } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
